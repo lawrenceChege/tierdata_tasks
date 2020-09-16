9e07f5334f248a73cb1a48a812be14f8a07e1715
lawrenceChege <lawrencechege.s@gmail.com>
2020-09-17 02:00:46 +0300

    change file permissions

---
+++ components/InputField.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="col-md-6">
+    <div class="form-group">
+      <input
+        type="text"
+        :value="value"
+        ...
+        @input="$emit('input', $event.target.value)"
+      />
+      <label for="name">Name <sub></sub></label>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      required: true,
+      type: String,
+    },
+  },
+  methods: {
+    handleChange(e) {
+      this.$emit('handleChange', e.target.value)
+    },
+  },
+}
+</script>
---
+++ pages/Tasks.vue
@@ -0,0 +1,39 @@
+<template>
+  <kanban-board :stages="stages" :blocks="blocks"> </kanban-board>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      stages: ['on-hold', 'in-progress', 'needs-review', 'approved'],
+      blocks: [
+        {
+          id: 1,
+          status: 'on-hold',
+          title: 'Welcome',
+        },
+        {
+          id: 2,
+          status: 'on-hold',
+          title: 'to',
+        },
+        {
+          id: 3,
+          status: 'in-progress',
+          title: 'the',
+        },
+        {
+          id: 4,
+          status: 'in-progress',
+          title: 'danger',
+        },
+        {
+          id: 5,
+          status: 'in-progress',
+          title: 'zone',
+        },
+      ],
+    }
+  },
+}
+</script>
